3e861302ea88dcf475446d2b5b2ee0b47af19618
Hariharan-SV <harikumar20112000@gmail.com>
2020-12-21 18:14:19 +0530

    product/Toolbar.js: Implement search

--- src/views/product/ProductListView/Toolbar.js
+++ src/views/product/ProductListView/Toolbar.js
@@ -9,9 +9,13 @@ import {
   TextField,
   InputAdornment,
   SvgIcon,
-  makeStyles
+  makeStyles,
+  ListItem,
+  ListItemText,
+  Typography
 } from '@material-ui/core';
 import { Search as SearchIcon } from 'react-feather';
+import {getResults} from '../../../services/game';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -25,34 +29,37 @@ const useStyles = makeStyles((theme) => ({
 
 const Toolbar = ({ className, ...rest }) => {
   const classes = useStyles();
+  const [name,setName] = React.useState("");
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const [matchingGames,setMatchingGames] = React.useState([]);
+
+  React.useEffect(()=>{
+    async function onLoad() {
+      let values = await getResults(name);
+      let data = [];
+      if(values['status'] === 1){
+        data = values['data'];
+      }
+      setMatchingGames(data);
+    }
+    onLoad();
+
+  },[matchingGames])
 
   return (
     <div
       className={clsx(classes.root, className)}
       {...rest}
     >
-      {/*<Box
-        display="flex"
-        justifyContent="flex-end"
-      >
-        <Button className={classes.importButton}>
-          Import
-        </Button>
-        <Button className={classes.exportButton}>
-          Export
-        </Button>
-        <Button
-          color="primary"
-          variant="contained"
-        >
-          Add product
-        </Button>
-      </Box>*/}
       <Box mt={3}>
         <Card>
           <CardContent>
-            <Box maxWidth={500}>
+            <Box maxWidth={600} style={{"backgroundColor":"#111"}}>
               <TextField
+                value={name}
+                onChange={handleNameChange}
                 fullWidth
                 InputProps={{
                   startAdornment: (
@@ -66,10 +73,16 @@ const Toolbar = ({ className, ...rest }) => {
                     </InputAdornment>
                   )
                 }}
-                placeholder="Search product"
+                placeholder="Search Game"
                 variant="outlined"
                 style={{backgroundColor:"#222"}}
               />
+              {matchingGames.map((game,index)=>(
+                <ListItem key={`Game ${index+1}`}>
+                  <ListItemText>{game['url_info']['url_name']}</ListItemText>
+                  <Typography variant="caption">{game['url_info']['type']}</Typography>
+                </ListItem>
+              ))}
             </Box>
           </CardContent>
         </Card>
