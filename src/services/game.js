0adbd0593e4725a95af0202d925259cb5948dd4b
Hariharan-SV <harikumar20112000@gmail.com>
2021-01-03 00:02:17 +0530

    Add function to add game to user searches

--- src/services/game.js
+++ src/services/game.js
@@ -38,4 +38,23 @@ const getGame = async (id) => {
   return result;
 };
 
-export {getResults, getGame};
+const addGametoSearches = async (id) => {
+  let result = {};
+  await fetch(process.env.REACT_APP_BACKEND_URL+'/game/add-to-search/'+id,{
+    method:'GET',
+    credentials: 'same-origin',
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin":"*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "x-auth-token":Cookie.get('x-auth-token'),
+    },
+  })
+  .then(response => response.json())
+  .then(res => result = res)
+  .catch(err=>console.log(err));
+  return result;
+};
+
+export {getResults, getGame, addGametoSearches};
